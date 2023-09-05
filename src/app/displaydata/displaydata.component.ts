b84d52cfe3e9a4304680358e656c689e10a5c337
Haider Bukhari <syed.haiderbukhari@corp.ibexglobal.com>
2023-09-05 14:38:22 +0500

    Input and Output Decorators

--- src/app/displaydata/displaydata.component.ts
+++ src/app/displaydata/displaydata.component.ts
@@ -3,9 +3,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-displaydata',
   template: `
+  <br>
   Username is: {{username}}<br>
-  Password is: {{password}}<br>
+  Password is: {{password}}<br><br>
   <button type="button" (click)="addNewUser(username)">Add to users, parent's list</button>
+  <br>
   `,
   styleUrls: ['./displaydata.component.css']
 })
